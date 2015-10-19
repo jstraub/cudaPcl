3260b231eaac825264844226097517089a5e9fdc
Julian Straub <jstraub@csail.mit.edu>
2015-10-19 11:04:18 -0400

    NYUcomputeNormals.py

---
+++ python/NYUcomputeNormals.py
@@ -0,0 +1,24 @@
+import os, re
+import subprocess as subp
+
+nyuPath = "/data/vision/fisher/data1/nyu_depth_v2/extracted/"
+
+names = []
+for root, dirs, files in os.walk(nyuPath):
+  for file in files:
+    name, ending = os.path.splitext(file)
+    if ending == '.png' and not re.search("_d",name) is None:
+      names.append(name)
+
+print "found ", len(names), " NYU files"
+
+
+for name in names:
+  print " ---- " + name
+  args = ["../build/bin/openniSmoothNormals_file",
+      "-i " + nyuPath + name + ".png",
+      "-o " + nyuPath + re.sub("_d", "", name)]
+  err = subp.call(" ".join(args), shell=True);
+  if err:
+    print "error when executing " + " ".join(args)
+  print " ---- " + name + " DONE -------"
